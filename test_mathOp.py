d8af2cf1c6c9df63a3aed32f82879909b663416a
Thirumalesh Hadapada Sreenivasa <hsthirumalesh@gmail.com>
2020-11-24 15:15:55 +0530

    Update test_mathOp.py

--- test_mathOp.py
+++ test_mathOp.py
@@ -4,6 +4,10 @@ from mathOp import factorial
 
 class ErrorsTestCase(unittest.TestCase):
 
+    def test_factorial_4(self):
+        factorial_4 = factorial(4)
+        self.assertTrue(factorial_4 == 24)    
+    
     def test_factorial_5(self):
         factorial_5 = factorial(5)
         self.assertTrue(factorial_5 == 120)
